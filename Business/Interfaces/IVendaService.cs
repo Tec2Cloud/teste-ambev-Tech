ba86c18f1491e9b1399d6120847e871820df4380
rodka82 <rduclerc@gmail.com>
2024-09-29 11:21:44 -0300

    feat: adding VendaService

---
+++ Business/Interfaces/IVendaService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IVendaService
+    {
+        Venda CriarVenda(Guid clienteId, string nomeCliente, Guid filialId, string nomeFilial, List<ItemVenda> itensDto);
+        Venda AtualizarVenda(Guid vendaId, List<ItemVenda> novosItensDto);
+        void CancelarVenda(Guid vendaId);
+        IEnumerable<Venda> ObterTodasVendas();
+        Venda? ObterVendaPorId(Guid vendaId);
+    }
+}
---
+++ Business/Services/VendaService.cs
@@ -0,0 +1,71 @@
+﻿using Business.Interfaces;
+using Data.Interfaces;
+using Domain.Entities;
+
+namespace Business.Services
+{
+    public class VendaService : IVendaService
+    {
+        private readonly IVendaRepository _vendaRepository; 
+
+        public VendaService(IVendaRepository vendaRepository)
+        {
+            _vendaRepository = vendaRepository;
+        }
+
+        public Venda CriarVenda(Guid clienteId, string nomeCliente, Guid filialId, string nomeFilial, List<ItemVenda> itensDto)
+        {
+            var venda = new Venda(clienteId, nomeCliente, filialId, nomeFilial);
+
+            foreach (var itemDto in itensDto)
+            {
+                venda.AdicionarItem(itemDto.ProdutoId, itemDto.NomeProduto, itemDto.ValorUnitario, itemDto.Quantidade, itemDto.Desconto);
+            }
+
+            _vendaRepository.Adicionar(venda);
+
+            return venda;
+        }
+
+    public Venda AtualizarVenda(Guid vendaId, List<ItemVenda> novosItensDto)
+    {
+        var venda = _vendaRepository.ObterPorId(vendaId);
+        if (venda == null)
+            throw new Exception("Venda não encontrada.");
+
+        venda.Itens.Clear();
+
+        foreach (var itemDto in novosItensDto)
+        {
+            venda.AdicionarItem(itemDto.ProdutoId, itemDto.NomeProduto, itemDto.ValorUnitario, itemDto.Quantidade, itemDto.Desconto);
+        }
+
+        venda.RecalcularValorTotal();
+
+        _vendaRepository.Atualizar(venda);
+
+        return venda;
+    }
+
+    public void CancelarVenda(Guid vendaId)
+        {
+            var venda = _vendaRepository.ObterPorId(vendaId);
+            if (venda == null)
+                throw new Exception("Venda não encontrada.");
+
+            venda.CancelarVenda();
+
+            _vendaRepository.Atualizar(venda);
+        }
+
+        public IEnumerable<Venda> ObterTodasVendas()
+        {
+            return _vendaRepository.ObterTodas();
+        }
+
+        public Venda? ObterVendaPorId(Guid vendaId)
+        {
+            return _vendaRepository.ObterPorId(vendaId);
+        }
+    }
+}
